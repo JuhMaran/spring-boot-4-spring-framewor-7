e52b34f99eeb418d0db7ce32fd44c7efc42cb212
JuhMaran <julianemaran@gmail.com>
2026-02-21 13:39:43 -0300

    Set Header on HTTP Response

--- spring-7-rest-mvc/src/main/java/guru/springframework/spring7restmvc/controller/BeerController.java
+++ spring-7-rest-mvc/src/main/java/guru/springframework/spring7restmvc/controller/BeerController.java
@@ -4,10 +4,10 @@ import guru.springframework.spring7restmvc.model.Beer;
 import guru.springframework.spring7restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -26,11 +26,24 @@ public class BeerController {
 
   private final BeerService beerService;
 
+  @PostMapping
+//  @RequestMapping(method = RequestMethod.POST)
+  public ResponseEntity handlePost(@RequestBody Beer beer) { // Raw use of parameterized class 'ResponseEntity'
+    Beer savedBeer = beerService.saveNewBeer(beer);
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.add("Location", "/api/v1/beer/" + savedBeer.getId().toString());
+
+    return new ResponseEntity(headers, HttpStatus.CREATED); // Raw use of parameterized class 'ResponseEntity' 
+  }
+
+  // Replace "@RequestMapping(method = RequestMethod.GET)" with "@GetMapping"
   @RequestMapping(method = RequestMethod.GET)
   public List<Beer> listBeers() {
     return beerService.listBeers();
   }
 
+  // Replace "@RequestMapping(method = RequestMethod.GET)" with "@GetMapping"
   @RequestMapping(method = RequestMethod.GET, value = "{beerId}")
   public Beer getBeerById(@PathVariable("beerId") UUID beerId) {
     log.debug("Get Beer by Id - in controller");
