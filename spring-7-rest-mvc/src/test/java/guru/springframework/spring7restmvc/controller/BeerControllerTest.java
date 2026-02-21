b3146ddf22b2dbc262c4568c8c9d29d1df4bba4d
JuhMaran <julianemaran@gmail.com>
2026-02-21 14:08:17 -0300

    Beer Test: Return Data with Mockito

--- spring-7-rest-mvc/src/test/java/guru/springframework/spring7restmvc/controller/BeerControllerTest.java
+++ spring-7-rest-mvc/src/test/java/guru/springframework/spring7restmvc/controller/BeerControllerTest.java
@@ -1,6 +1,8 @@
 package guru.springframework.spring7restmvc.controller;
 
+import guru.springframework.spring7restmvc.model.Beer;
 import guru.springframework.spring7restmvc.services.BeerService;
+import guru.springframework.spring7restmvc.services.BeerServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
@@ -10,11 +12,14 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
 @WebMvcTest(BeerController.class)
-//@SpringBootTest
 class BeerControllerTest {
 
   @Autowired
@@ -23,15 +28,19 @@ class BeerControllerTest {
   @MockitoBean
   BeerService beerService;
 
-//  @Autowired
-//  BeerController beerController;
+  BeerServiceImpl beerServiceImpl = new BeerServiceImpl();
 
   @Test
   void getBeerById() throws Exception {
-//    System.out.println(beerController.getBeerById(UUID.randomUUID()));
-    mockMvc.perform(get("" + UUID.randomUUID())
+
+    Beer testBeer = beerServiceImpl.listBeers().getFirst();
+
+    given(beerService.getBeerById(any(UUID.class))).willReturn(testBeer);
+
+    mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID())
         .accept(MediaType.APPLICATION_JSON))
-      .andExpect(status().isOk());
+      .andExpect(status().isOk())
+      .andExpect(content().contentType(MediaType.APPLICATION_JSON));
   }
 
 }
