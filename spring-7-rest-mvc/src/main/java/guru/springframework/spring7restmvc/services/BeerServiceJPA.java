c9b3bc780597c0a18c9436a354039f034bcfb164
JuhMaran <julianemaran@gmail.com>
2026-02-22 14:06:55 -0300

    JPA Save New Beer Operation

--- spring-7-rest-mvc/src/main/java/guru/springframework/spring7restmvc/services/BeerServiceJPA.java
+++ spring-7-rest-mvc/src/main/java/guru/springframework/spring7restmvc/services/BeerServiceJPA.java
@@ -41,7 +41,7 @@ public class BeerServiceJPA implements BeerService {
 
   @Override
   public BeerDTO saveNewBeer(BeerDTO beerDTO) {
-    return null;
+    return beerMapper.beerToBeerDto(beerRepository.save(beerMapper.beerDtoToBeer(beerDTO)));
   }
 
   @Override
