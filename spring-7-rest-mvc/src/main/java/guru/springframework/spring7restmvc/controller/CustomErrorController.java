b3be3b5404322aa6ecc46954e8bcc1371572b646
JuhMaran <julianemaran@gmail.com>
2026-02-22 19:08:15 -0300

    Custom Error Body

--- spring-7-rest-mvc/src/main/java/guru/springframework/spring7restmvc/controller/CustomErrorController.java
+++ spring-7-rest-mvc/src/main/java/guru/springframework/spring7restmvc/controller/CustomErrorController.java
@@ -2,21 +2,33 @@ package guru.springframework.spring7restmvc.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
- * spring-7-rest-mvc
+ * Custom Error Body
  *
  * @author Juliane Maran
  * @since 22/02/2026
  */
-@ControllerAdvice
+@RestControllerAdvice
 public class CustomErrorController {
 
   @ExceptionHandler(MethodArgumentNotValidException.class)
   ResponseEntity handleBindErrors(MethodArgumentNotValidException exception) {
-    return ResponseEntity.badRequest().body(exception.getBindingResult().getFieldErrors());
+
+    List errorList = exception.getFieldErrors().stream()
+      .map(fieldError -> {
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put(fieldError.getField(), fieldError.getDefaultMessage());
+        return errorMap;
+      }).toList();
+
+    return ResponseEntity.badRequest().body(errorList);
   }
 
 }
