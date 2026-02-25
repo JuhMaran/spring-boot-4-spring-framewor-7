a7c45cdd21b012ac6e9a8c05207cd07ecaf348e2
JuhMaran <julianemaran@gmail.com>
2026-02-25 15:56:31 -0300

    List Beers Spring MVC Test

--- spring-7-rest-mvc/src/test/java/guru/springframework/spring7restmvc/controller/BeerControllerIT.java
+++ spring-7-rest-mvc/src/test/java/guru/springframework/spring7restmvc/controller/BeerControllerIT.java
@@ -27,6 +27,7 @@ import java.util.UUID;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -60,17 +61,30 @@ class BeerControllerIT {
   }
 
   @Test
+  void testListBeersByName() throws Exception {
+    mockMvc.perform(get(BeerController.BEER_PATH)
+        .queryParam("beerName", "IPA"))
+      .andExpect(status().isOk())
+      .andExpect(jsonPath("$.size()", is(100)));
+
+  }
+
+  @Test
   void testPatchBeerBadName() throws Exception {
     Beer beer = beerRepository.findAll().getFirst();
 
     Map<String, Object> beerMap = new HashMap<>();
     beerMap.put("beerName", "New Name 1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890");
 
-    mockMvc.perform(patch(BeerController.BEER_PATH_ID, beer.getId())
+    MvcResult result = mockMvc.perform(patch(BeerController.BEER_PATH_ID, beer.getId())
         .contentType(MediaType.APPLICATION_JSON)
         .accept(MediaType.APPLICATION_JSON)
         .content(objectMapper.writeValueAsString(beerMap)))
-      .andExpect(status().isBadRequest());
+      .andExpect(status().isBadRequest())
+      .andExpect(jsonPath("$.length()", is(1)))
+      .andReturn();
+
+    System.out.println(result.getResponse().getContentAsString());
 
   }
 
@@ -128,7 +142,7 @@ class BeerControllerIT {
     assertThat(responseEntity.getHeaders().getLocation()).isNotNull();
 
     String[] locationUUID = responseEntity.getHeaders().getLocation().getPath().split("/");
-    UUID savedUUID = UUID.fromString(locationUUID[4]);
+    UUID savedUUID = UUID.fromString(locationUUID[ 4 ]);
 
     Beer beer = beerRepository.findById(savedUUID).get();
     assertThat(beer).isNotNull();
