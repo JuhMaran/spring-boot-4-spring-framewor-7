1defa9e8b0956140688298ec25bab05a0a5c9711
JuhMaran <julianemaran@gmail.com>
2026-02-27 09:52:43 -0300

    Persisting Beer Order Relationships

--- spring-7-rest-mvc/src/test/java/guru/springframework/spring7restmvc/repositories/BeerOrderRepositoryTest.java
+++ spring-7-rest-mvc/src/test/java/guru/springframework/spring7restmvc/repositories/BeerOrderRepositoryTest.java
@@ -1,11 +1,13 @@
 package guru.springframework.spring7restmvc.repositories;
 
 import guru.springframework.spring7restmvc.entities.Beer;
+import guru.springframework.spring7restmvc.entities.BeerOrder;
 import guru.springframework.spring7restmvc.entities.Customer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 //@DataJpaTest
 @SpringBootTest
@@ -29,12 +31,16 @@ class BeerOrderRepositoryTest {
     testBeer = beerRepository.findAll().get(0);
   }
 
+  @Transactional
   @Test
   void testBeerOrders() {
-    System.out.println(beerOrderRepository.count());
-    System.out.println(customerRepository.count());
-    System.out.println(beerRepository.count());
-    System.out.println(testCustomer.getName());
-    System.out.println(testBeer.getBeerName());
+    BeerOrder beerOrder = BeerOrder.builder()
+      .customerRef("Test order")
+      .customer(testCustomer)
+      .build();
+
+    BeerOrder savedBeerOrder = beerOrderRepository.saveAndFlush(beerOrder);
+    System.out.println(savedBeerOrder.getCustomerRef());
   }
+
 }
