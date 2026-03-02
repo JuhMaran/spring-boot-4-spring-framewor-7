02d1e3aa270040a5cc18f8e5dc592268d63ca930
JuhMaran <julianemaran@gmail.com>
2026-03-01 21:47:44 -0300

    add sdjpa project

---
+++ sdjpa-springdatarest/src/main/java/org/springframework/sdjpa/SdjpaSpringdatarestApplication.java
@@ -0,0 +1,13 @@
+package org.springframework.sdjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdjpaSpringdatarestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdjpaSpringdatarestApplication.class, args);
+	}
+
+}
---
+++ sdjpa-springdatarest/src/test/java/org/springframework/sdjpa/SdjpaSpringdatarestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springframework.sdjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdjpaSpringdatarestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
