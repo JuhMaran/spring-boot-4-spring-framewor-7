9e3caa7dcf223f1b80b74c0ff2e8490510870542
JuhMaran <julianemaran@gmail.com>
2026-03-02 18:23:45 -0300

    Spring RestTemplate - Project Code Review

---
+++ spring-7-resttemplate/src/main/java/guru/springframework/resttemplate/model/BeerDTO.java
@@ -0,0 +1,30 @@
+package guru.springframework.resttemplate.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/**
+ * DTO
+ *
+ * @author Juliane Maran
+ * @since 02/03/2026
+ */
+@Data
+@Builder
+public class BeerDTO {
+
+  private UUID id;
+  private Integer version;
+  private String beerName;
+  private BeerStyle beerStyle;
+  private String upc;
+  private Integer quantityOnHand;
+  private BigDecimal price;
+  private LocalDateTime createdDate;
+  private LocalDateTime updateDate;
+
+}
