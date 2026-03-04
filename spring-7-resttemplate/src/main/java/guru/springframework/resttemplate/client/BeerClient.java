f119b8a0b7ae6426864e2a8d857e9c019e79f6bf
JuhMaran <julianemaran@gmail.com>
2026-03-04 13:41:12 -0300

    HTTP Post with RestTemplate

--- spring-7-resttemplate/src/main/java/guru/springframework/resttemplate/client/BeerClient.java
+++ spring-7-resttemplate/src/main/java/guru/springframework/resttemplate/client/BeerClient.java
@@ -21,4 +21,6 @@ public interface BeerClient {
 
   BeerDTO getBeerById(UUID beerId);
 
+  BeerDTO createBeer(BeerDTO newDto);
+
 }
--- spring-7-resttemplate/src/test/java/guru/springframework/resttemplate/client/BeerClientImplTest.java
+++ spring-7-resttemplate/src/test/java/guru/springframework/resttemplate/client/BeerClientImplTest.java
@@ -1,10 +1,13 @@
 package guru.springframework.resttemplate.client;
 
 import guru.springframework.resttemplate.model.BeerDTO;
+import guru.springframework.resttemplate.model.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
+
+import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -13,6 +16,20 @@ class BeerClientImplTest {
 
   @Autowired
   BeerClientImpl beerClient;
+
+  @Test
+  void testCreateBeer() {
+    BeerDTO newDto = BeerDTO.builder()
+      .price(new BigDecimal("10.99"))
+      .beerName("Mango Bobs")
+      .beerStyle(BeerStyle.IPA)
+      .quantityOnHand(500)
+      .upc("12345")
+      .build();
+
+    BeerDTO savedDto = beerClient.createBeer(newDto);
+    assertNotNull(savedDto);
+  }
 
   @Test
   void getBeerById() {
