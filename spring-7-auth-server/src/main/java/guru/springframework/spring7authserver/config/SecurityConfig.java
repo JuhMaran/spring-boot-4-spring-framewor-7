5b26f0ea8fc3de2eff7a632c5cabb2873cc2fd7a
JuhMaran <julianemaran@gmail.com>
2026-03-06 15:56:33 -0300

    Add Authorization Server Filter Chain

---
+++ spring-7-auth-server/src/main/java/guru/springframework/spring7authserver/config/SecurityConfig.java
@@ -0,0 +1,59 @@
+package guru.springframework.spring7authserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.MediaType;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
+
+/**
+ * Spring Security Configuration
+ *
+ * @author Juliane Maran
+ * @since 06/03/2026
+ */
+@Configuration
+public class SecurityConfig {
+
+  @Bean
+  @Order(1)
+  public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http)
+    throws Exception {
+    OAuth2AuthorizationServerConfigurer authorizationServerConfigurer =
+      OAuth2AuthorizationServerConfigurer.authorizationServer();
+
+    http
+      .securityMatcher(authorizationServerConfigurer.getEndpointsMatcher())
+
+      .with(authorizationServerConfigurer, authorizationServer ->
+        authorizationServer
+          .oidc(Customizer.withDefaults()) // Enable OpenID Connect 1.0
+      )
+
+      .authorizeHttpRequests(authorize ->
+        authorize
+          .anyRequest().authenticated()
+      )
+
+      // Redireciona para /login quando o usuário não está autenticado
+      .exceptionHandling(exceptions -> exceptions
+        .defaultAuthenticationEntryPointFor(
+          new LoginUrlAuthenticationEntryPoint("/login"),
+          new MediaTypeRequestMatcher(MediaType.TEXT_HTML)
+        )
+      )
+
+      // Suporte a JWT para Resource Server
+      .oauth2ResourceServer(oauth2 ->
+        oauth2.jwt(Customizer.withDefaults())
+      );
+
+    return http.build();
+  }
+
+}
