d63c18634deddacdc45e7e414f2fbfd9ce6948b1
JuhMaran <julianemaran@gmail.com>
2026-03-09 12:58:07 -0300

    test: Mono Operations

---
+++ spring-7-reactive/src/test/java/guru/springframework/spring7reactive/repositories/PersonRepositoryImplTest.java
@@ -0,0 +1,41 @@
+package guru.springframework.spring7reactive.repositories;
+
+import guru.springframework.spring7reactive.domain.Person;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+
+class PersonRepositoryImplTest {
+
+  PersonRepository personRepository = new PersonRepositoryImpl();
+
+  @Test
+  void testMonoByIdBlock() {
+    Mono<Person> personMono = personRepository.getById(1);
+
+    Person person = personMono.block();
+
+    System.out.println(person.toString());
+  }
+
+  @Test
+  void testGetByIdSubscriber() {
+    Mono<Person> personMono = personRepository.getById(1);
+
+    personMono.subscribe(person -> {
+      System.out.println(person.toString());
+    });
+  }
+
+  @Test
+  void testMapOperation() {
+    Mono<Person> personMono = personRepository.getById(1);
+
+    personMono.map(person -> {
+      return person.getFirstName();
+    }).subscribe(firstName -> {
+      System.out.println(firstName);
+    });
+
+  }
+
+}
