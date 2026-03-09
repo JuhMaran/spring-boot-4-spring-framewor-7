3badde2bc092a7f5c60e7b8a0566bb2de5197439
JuhMaran <julianemaran@gmail.com>
2026-03-09 14:43:45 -0300

    assignment 26: Implement Find By Id

--- spring-7-reactive/src/test/java/guru/springframework/spring7reactive/repositories/PersonRepositoryImplTest.java
+++ spring-7-reactive/src/test/java/guru/springframework/spring7reactive/repositories/PersonRepositoryImplTest.java
@@ -7,9 +7,25 @@ import reactor.core.publisher.Mono;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class PersonRepositoryImplTest {
 
   PersonRepository personRepository = new PersonRepositoryImpl();
+
+  @Test
+  void testGetByIdFound() {
+    Mono<Person> personMono = personRepository.getById(3);
+
+    assertEquals(Boolean.TRUE, personMono.hasElement().block());
+  }
+
+  @Test
+  void testGetByIdNotFound() {
+    Mono<Person> personMono = personRepository.getById(7);
+
+    assertEquals(Boolean.FALSE, personMono.hasElement().block());
+  }
 
   @Test
   void testMonoByIdBlock() {
