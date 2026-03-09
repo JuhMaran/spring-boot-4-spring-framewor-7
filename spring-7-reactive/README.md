086ea9963d560f91fa0dbfd3c16eec4746869bc0
JuhMaran <julianemaran@gmail.com>
2026-03-09 12:45:05 -0300

    Implement Repository

--- spring-7-reactive/README.md
+++ spring-7-reactive/README.md
@@ -2,10 +2,11 @@
 
 ## Tecnologias
 
-* Spring Framework 7+
-* Spring Boot 4.0.3
-* Java SE 25 LTS
-* Spring Reactive Web (WebFlux) 4.0.3
-* Project Lombok 1.18.42
+* **Spring Framework 7** (Nov/2025)
+* **Spring Boot 4** (Nov/2025)
+* **Java SE 25 LTS** (Oracle — 16/09/2025)
+* **Jackson 3.1.0** (tools.jackson.core) (Feb/2026)
+* **Spring Reactive Web (WebFlux) 4.0.3**
+* **Project Lombok 1.18.42**
 
 ---
---
+++ spring-7-reactive/src/main/java/guru/springframework/spring7reactive/domain/Person.java
@@ -0,0 +1,24 @@
+package guru.springframework.spring7reactive.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * spring-7-reactive
+ *
+ * @author Juliane Maran
+ * @since 09/03/2026
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+
+  private Integer id;
+  private String firstName;
+  private String lastName;
+
+}
