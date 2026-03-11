0f8ad47e7e45b42b5ccff2ab3888e7c932b5bf47
JuhMaran <julianemaran@gmail.com>
2026-03-11 14:29:01 -0300

    Test Update Beer Entity

--- spring-7-reactive/src/main/java/guru/springframework/spring7reactive/controllers/BeerController.java
+++ spring-7-reactive/src/main/java/guru/springframework/spring7reactive/controllers/BeerController.java
@@ -35,14 +35,14 @@ public class BeerController {
   Mono<ResponseEntity<Void>> patchExistingBeer(@PathVariable Integer beerId,
                                                @Validated @RequestBody BeerDTO beerDTO) {
     return beerService.patchBeer(beerId, beerDTO)
-      .map(updatedDto -> ResponseEntity.ok().build());
+      .map(updatedDto -> ResponseEntity.noContent().build());
   }
 
   @PutMapping(BEER_PATH_ID)
   Mono<ResponseEntity<Void>> updateExistingBeer(@PathVariable("beerId") Integer beerId,
                                                 @Validated @RequestBody BeerDTO beerDTO) {
     return beerService.updateBeer(beerId, beerDTO)
-      .map(savedDto -> ResponseEntity.ok().build());
+      .map(savedDto -> ResponseEntity.noContent().build());
   }
 
   @PostMapping(BEER_PATH)
