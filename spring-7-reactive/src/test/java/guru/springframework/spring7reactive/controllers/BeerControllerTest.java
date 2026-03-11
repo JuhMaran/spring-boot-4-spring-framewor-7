a39dbc699521c571f0c0649b98e082c0ddfe9456
JuhMaran <julianemaran@gmail.com>
2026-03-11 14:03:47 -0300

    Test List Beers

---
+++ spring-7-reactive/src/test/java/guru/springframework/spring7reactive/controllers/BeerControllerTest.java
@@ -0,0 +1,24 @@
+package guru.springframework.spring7reactive.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webtestclient.autoconfigure.AutoConfigureWebTestClient;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest
+@AutoConfigureWebTestClient
+class BeerControllerTest {
+
+  @Autowired
+  WebTestClient webTestClient;
+
+  @Test
+  void testListBeers() {
+    webTestClient.get()
+      .uri(BeerController.BEER_PATH).exchange()
+      .expectStatus().isOk()
+      .expectHeader().valueEquals("Content-Type", "application/json")
+      .expectBody().jsonPath("$.size()").isEqualTo(3);
+  }
+}
