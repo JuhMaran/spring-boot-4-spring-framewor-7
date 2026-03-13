ec9e3b61eac99d826eb74480fe0d83e7d8aac476
JuhMaran <julianemaran@gmail.com>
2026-03-12 22:43:10 -0300

    Implement CRUD Operations

--- spring-7-reactive-mongo/src/main/java/guru/springframework/spring7reactivemongo/services/BeerService.java
+++ spring-7-reactive-mongo/src/main/java/guru/springframework/spring7reactivemongo/services/BeerService.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring7reactivemongo.services;
 
 import guru.springframework.spring7reactivemongo.model.BeerDTO;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 /**
@@ -11,8 +12,18 @@ import reactor.core.publisher.Mono;
  */
 public interface BeerService {
 
-  Mono<BeerDTO> saveBeer(Mono<BeerDTO> beerDTO);
+  Flux<BeerDTO> listBeers();
+
+  Mono<BeerDTO> saveBeer(Mono<BeerDTO> beerDto);
+
+  Mono<BeerDTO> saveBeer(BeerDTO beerDTO);
 
   Mono<BeerDTO> getById(String beerId);
+
+  Mono<BeerDTO> updateBeer(String beerId, BeerDTO beerDTO);
+
+  Mono<BeerDTO> patchBeer(String beerId, BeerDTO beerDTO);
+
+  Mono<Void> deleteBeerById(String beerId);
 
 }
