c86f4f41118e95caf8548e6d96ad88f70bf5fb9c
JuhMaran <julianemaran@gmail.com>
2026-03-14 13:10:58 -0300

    add new project: spring-7-webclient

---
+++ spring-7-webclient/src/main/java/guru/springframework/spring7webclient/Spring7WebclientApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring7webclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring7WebclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring7WebclientApplication.class, args);
+	}
+
+}
---
+++ spring-7-webclient/src/test/java/guru/springframework/spring7webclient/Spring7WebclientApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring7webclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring7WebclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
