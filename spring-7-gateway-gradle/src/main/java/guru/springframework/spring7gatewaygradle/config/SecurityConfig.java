5cc14acdf4855e9bd8c38efe5535f2a3a62e2e63
JuhMaran <julianemaran@gmail.com>
2026-03-16 14:29:14 -0300

    Add Gradle

---
+++ spring-7-gateway-gradle/src/main/java/guru/springframework/spring7gatewaygradle/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package guru.springframework.spring7gatewaygradle.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+/**
+ * spring-7-gateway-gradle
+ *
+ * @author Juliane Maran
+ * @since 16/03/2026
+ */
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+  @Bean
+  public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+
+    http.authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().authenticated())
+      .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec.jwt(Customizer.withDefaults()))
+      .csrf(ServerHttpSecurity.CsrfSpec::disable);
+
+    return http.build();
+  }
+
+}
---
+++ spring-7-gateway-gradle/src/test/java/guru/springframework/spring7gatewaygradle/ApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring7gatewaygradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
