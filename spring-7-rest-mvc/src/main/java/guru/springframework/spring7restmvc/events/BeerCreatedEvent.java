e50a7feba75d9ab06f0caa12123186a68c2dc9b4
JuhMaran <julianemaran@gmail.com>
2026-03-22 12:52:47 -0300

    adding event POJO

---
+++ spring-7-rest-mvc/src/main/java/guru/springframework/spring7restmvc/events/BeerCreatedEvent.java
@@ -0,0 +1,25 @@
+package guru.springframework.spring7restmvc.events;
+
+import guru.springframework.spring7restmvc.entities.Beer;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.Authentication;
+
+/**
+ * spring-7-rest-mvc
+ *
+ * @author Juliane Maran
+ * @since 22/03/2026
+ */
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+public class BeerCreatedEvent {
+
+  private Beer beer;
+  private Authentication authentication;
+
+}
