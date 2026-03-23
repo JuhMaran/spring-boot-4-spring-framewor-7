b4a7dedb618620976dd3c869f8e6be6b0da3b4a3
JuhMaran <julianemaran@gmail.com>
2026-03-23 17:04:11 -0300

    persisting audit event

--- spring-7-rest-mvc/src/main/java/guru/springframework/spring7restmvc/listeners/BeerCreatedListener.java
+++ spring-7-rest-mvc/src/main/java/guru/springframework/spring7restmvc/listeners/BeerCreatedListener.java
@@ -1,6 +1,11 @@
 package guru.springframework.spring7restmvc.listeners;
 
 import guru.springframework.spring7restmvc.events.BeerCreatedEvent;
+import guru.springframework.spring7restmvc.mappers.BeerMapper;
+import guru.springframework.spring7restmvc.repositories.BeerAuditRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
@@ -11,19 +16,27 @@ import org.springframework.stereotype.Component;
  * @author Juliane Maran
  * @since 22/03/2026
  */
+@Slf4j
 @Component
+@RequiredArgsConstructor
 public class BeerCreatedListener {
+
+  private final BeerMapper beerMapper;
+  private final BeerAuditRepository beerAuditRepository;
 
   @Async
   @EventListener
   public void listen(BeerCreatedEvent event) {
-    System.out.println("I heard a beer was created!");
-    System.out.println(event.getBeer().getId());
+    val beerAudit = beerMapper.beerToBeerAudit(event.getBeer());
+    beerAudit.setAuditEventType("BEER_CREATED");
 
-    System.out.println("Current Thread Name: " + Thread.currentThread().getName());
-    System.out.println("Current Thread ID: " + Thread.currentThread().getId());
+    if (event.getAuthentication() != null && event.getAuthentication().getName() != null) {
+      beerAudit.setPrincipalName(event.getAuthentication().getName());
+    }
 
-    //todo impl - add real implementation to persist audit record
+    val savedBeerAudit = beerAuditRepository.save(beerAudit);
+    log.debug("Beer Audit Saved: {}", savedBeerAudit.getId());
+
   }
 
 }
