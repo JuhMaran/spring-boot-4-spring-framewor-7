64dc3ff04a0822c43bb7db67bfeedf10605b7ca8
JuhMaran <julianemaran@gmail.com>
2026-03-27 20:07:24 -0300

    adding order line status

---
+++ spring-7-rest-mvc/src/main/java/guru/springframework/spring7restmvc/model/BeerOrderLineStatus.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring7restmvc.model;
+
+/**
+ * spring-7-rest-mvc
+ *
+ * @author Juliane Maran
+ * @since 27/03/2026
+ */
+public enum BeerOrderLineStatus {
+
+  NEW, COMPLETE
+
+}
